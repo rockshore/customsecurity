e2c79b32a8d50e4da6031d2d4b59830ea95fcdfb
neildunlop <neil.dunlop@gmail.com>
2015-02-03 13:40:59 +0000

    Moved tests to appropriate files.

--- src/test/java/net/rockshore/prototype/customsecurity/core/net/rockshore/prototype/customsecurity/core/extendedproperties/ExtendedPropertiesTests.java
+++ src/test/java/net/rockshore/prototype/customsecurity/core/net/rockshore/prototype/customsecurity/core/extendedproperties/ExtendedPropertiesTests.java
@@ -1,6 +1,5 @@
 package net.rockshore.prototype.customsecurity.core.net.rockshore.prototype.customsecurity.core.extendedproperties;
 
-import junit.framework.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,7 +8,6 @@ import java.util.List;
 import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
 
 /**
  * Created by neildunlop on 02/02/15.
@@ -75,72 +73,5 @@ public class ExtendedPropertiesTests {
         assertEquals(newShoeSize, extendedProperties.get(shoeSizeKey));
     }
 
-    @Test
-    public void shouldThrowExceptionIfPropertyNameIsNull() {
 
-        String propertyName = null;
-
-        try {
-            ExtendedPropertyDefinition definition = new ExtendedPropertyDefinition(propertyName, String.class, "default", true);
-            fail("Expected exception was not thrown.");
-        } catch (IllegalArgumentException e) {
-            assertEquals("Cannot create an extended property with a null or empty name.", e.getMessage());
-        }
-    }
-
-    @Test
-    public void shouldThrowExceptionIfPropertyNameIsEmpty() {
-
-        String propertyName = "   ";
-
-        try {
-            ExtendedPropertyDefinition definition = new ExtendedPropertyDefinition(propertyName, String.class, "default", true);
-            fail("Expected exception was not thrown.");
-        } catch (IllegalArgumentException e) {
-            assertEquals("Cannot create an extended property with a null or empty name.", e.getMessage());
-        }
-    }
-
-    @Test
-    public void shouldThrowExceptionWhenNullDataTypeIsSupplied() {
-
-        Class dataType = null;
-
-        try {
-            ExtendedPropertyDefinition definition = new ExtendedPropertyDefinition("Test Property", dataType, "default", true);
-            fail("Expected exception was not thrown.");
-        }
-        catch (IllegalArgumentException e) {
-            assertEquals("Cannot create an extended property with a null data type.", e.getMessage());
-        }
-    }
-
-    @Test
-    public void shouldThrowExceptionWhenNullDefaultValueIsSuppliedButNullAllowedIsFalse() {
-
-        String defaultValue = null;
-
-        try {
-            ExtendedPropertyDefinition definition = new ExtendedPropertyDefinition("Test Property", String.class, defaultValue, false);
-            fail("Expected exception was not thrown.");
-        }
-        catch (IllegalArgumentException e) {
-            assertEquals("Cannot set value of extended property to null.  Null is not an allowed value.", e.getMessage());
-        }
-    }
-
-    @Test
-    public void shouldThrowExceptionDefaultValueTypeDoesNotMatchSuppliedDataType() {
-
-        String defaultValue = "Hello";
-        Class dataType = Integer.class;
-
-        try {
-            ExtendedPropertyDefinition definition = new ExtendedPropertyDefinition("Test Property", dataType, defaultValue, false);
-            fail("Expected exception was not thrown.");
-        }
-        catch (IllegalArgumentException e) {
-            assertEquals("Cannot set value of extended property.  The value '" + defaultValue + "'is not of the expected type '" + dataType.getName().toString() + "'.", e.getMessage());
-        }
-    }
 }
--- src/test/java/net/rockshore/prototype/customsecurity/core/net/rockshore/prototype/customsecurity/core/extendedproperties/ExtendedPropertyDefinitionTests.java
+++ src/test/java/net/rockshore/prototype/customsecurity/core/net/rockshore/prototype/customsecurity/core/extendedproperties/ExtendedPropertyDefinitionTests.java
@@ -1,8 +1,82 @@
 package net.rockshore.prototype.customsecurity.core.net.rockshore.prototype.customsecurity.core.extendedproperties;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 /**
  * Created by neildunlop on 03/02/15.
+ * Tests to confirm that Extended Property Definitions work as expected.
  */
 public class ExtendedPropertyDefinitionTests {
 
+    @Test
+    public void shouldThrowExceptionIfPropertyNameIsNull() {
+
+        String propertyName = null;
+
+        try {
+            ExtendedPropertyDefinition definition = new ExtendedPropertyDefinition(propertyName, String.class, "default", true);
+            fail("Expected exception was not thrown.");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Cannot create an extended property with a null or empty name.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldThrowExceptionIfPropertyNameIsEmpty() {
+
+        String propertyName = "   ";
+
+        try {
+            ExtendedPropertyDefinition definition = new ExtendedPropertyDefinition(propertyName, String.class, "default", true);
+            fail("Expected exception was not thrown.");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Cannot create an extended property with a null or empty name.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenNullDataTypeIsSupplied() {
+
+        Class dataType = null;
+
+        try {
+            ExtendedPropertyDefinition definition = new ExtendedPropertyDefinition("Test Property", dataType, "default", true);
+            fail("Expected exception was not thrown.");
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("Cannot create an extended property with a null data type.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenNullDefaultValueIsSuppliedButNullAllowedIsFalse() {
+
+        String defaultValue = null;
+
+        try {
+            ExtendedPropertyDefinition definition = new ExtendedPropertyDefinition("Test Property", String.class, defaultValue, false);
+            fail("Expected exception was not thrown.");
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("Cannot set value of extended property to null.  Null is not an allowed value.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldThrowExceptionDefaultValueTypeDoesNotMatchSuppliedDataType() {
+
+        String defaultValue = "Hello";
+        Class dataType = Integer.class;
+
+        try {
+            ExtendedPropertyDefinition definition = new ExtendedPropertyDefinition("Test Property", dataType, defaultValue, false);
+            fail("Expected exception was not thrown.");
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("Cannot set value of extended property.  The value '" + defaultValue + "'is not of the expected type '" + dataType.getName().toString() + "'.", e.getMessage());
+        }
+    }
 }
